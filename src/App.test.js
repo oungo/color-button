2844f20eae1f683cd30bc6e85fa163570e4f88d6
oungo <y2k6273@naver.com>
2023-01-22 17:43:59 +0900

    15. 버튼 클릭 시 테스트 행동(Test Behavior when Clicking Button)

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial color', () => {
+test('button has correct initial color, and updates when clicked', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent('Change to red');
 });
 
-test('button turns blue when clicked', () => {
 
-});
