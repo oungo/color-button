1133863ad641e0f33cdde7eb8d6b1754f3c1873f
oungo <y2k6273@naver.com>
2023-01-22 17:36:51 +0900

    13. Color Button 앱 시작하기

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+});
+
+test('button turns blue when clicked', () => {
+
 });
